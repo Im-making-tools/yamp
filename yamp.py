d8d5e443a0d29f1a0878c555fa32c79c129af0b1
Im-making-tools <im_making_tools@infcof.com>
2026-07-09 20:15:49 +0100

    More fixes for random exceptions

--- yamp.py
+++ yamp.py
@@ -715,6 +715,9 @@ class MainLauncher:
                             self.log.info(f"Updating [green]{data['name']}")
                         elif old_name != data['name']:
                             self.log.info(f"Updating [blue]{old_name}[/blue] -> [green]{data['name']}")
+                    except RuntimeError:  # Exception with the coroute
+                        self.log.error(f"Fetching [gray50]{rid}[/gray50] raised internal error. Opt={opt}")
+                        error = True
                     except asyncio.TimeoutError:
                         self.log.error(f"Fetching [gray50]{rid}[/gray50] timed out")
                         error = True
